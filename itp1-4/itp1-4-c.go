85d5bcf761a49a34f8d45394e18be3676c670183
yanutetsu <satoshi.yanuma@topgate.co.jp>
2018-08-17 10:34:56 +0900

    refactor nextInt func and create nextChar func

--- itp1-4/itp1-4-c.go
+++ itp1-4/itp1-4-c.go
@@ -7,24 +7,24 @@ import (
 	"strconv"
 )
 
-var sc = bufio.NewScanner(os.Stdin)
-
-func nextInt() (int, error) {
+func nextChar(sc *bufio.Scanner) string {
 	sc.Scan()
-	s := sc.Text()
-	return strconv.Atoi(s)
+	return sc.Text()
+}
+
+func nextInt(sc *bufio.Scanner) int {
+	i, _ := strconv.Atoi(nextChar(sc))
+	return i
 }
 
 func main() {
+	sc := bufio.NewScanner(os.Stdin)
 	sc.Split(bufio.ScanWords)
 
-	n := nextInt()
-
-	for i := 0; i < n; i++ {
-		a, _ := nextInt()
-		sc.Scan()
-		op := sc.Text()
-		b, _ := nextInt()
+	for {
+		a := nextInt(sc)
+		op := nextChar(sc)
+		b := nextInt(sc)
 
 		switch op {
 		case "?":
